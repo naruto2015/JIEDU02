bc6ace91181294ece783c57459bacc699498581e
naruto2015 <995804286@qq.com>
2016-07-08 16:08:25 +0800

    12

--- app/src/main/java/com/myandroidview/activity/MainActivity.java
+++ app/src/main/java/com/myandroidview/activity/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//
 
         EventBus.getDefault().register(this);
 

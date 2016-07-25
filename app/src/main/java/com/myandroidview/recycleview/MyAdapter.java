de436d57e50a6ff740359196363cd9d0dbd08ca8
naruto2015 <995804286@qq.com>
2016-07-25 11:11:47 +0800

    gds

--- app/src/main/java/com/myandroidview/recycleview/MyAdapter.java
+++ app/src/main/java/com/myandroidview/recycleview/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.myandroidview.recycleview;
 
 import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,15 +28,32 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         int type=getItemViewType(viewType);
         View view=null;
-        if(type==1){
+        if(type==0){
             view=LayoutInflater.from(context).inflate(R.layout.headview,parent,false);
 
-        }else if(type==2){
-            LayoutInflater.from(context).inflate(R.layout.recycleview_item,parent,false);
+        }else if(type==1){
+            view=LayoutInflater.from(context).inflate(R.layout.recycleview_item,parent,false);
 
         }
 
         return new Holder(view);
+    }
+
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        //super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager layoutManager=recyclerView.getLayoutManager();
+        if(layoutManager instanceof GridLayoutManager){
+            final GridLayoutManager gridLayoutManager= (GridLayoutManager) layoutManager;
+            gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+
+                    return getItemViewType(position)==0?gridLayoutManager.getSpanCount():1;
+                }
+            });
+        }
     }
 
     @Override
--- app/src/main/java/com/myandroidview/recycleview/RecycleViewDemoActivity.java
+++ app/src/main/java/com/myandroidview/recycleview/RecycleViewDemoActivity.java
@@ -1,15 +1,71 @@
 package com.myandroidview.recycleview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.myandroidview.activity.R;
+import com.myandroidview.bean.RecycleViewBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.DividerItemDecoration;
 
 public class RecycleViewDemoActivity extends AppCompatActivity {
 
+
+    private RecyclerView recyclerView;
+    private List<RecycleViewBean> list=new ArrayList<>();
+    private Context context;
+    private MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view_demo);
+        context=this;
+
+        init();
+
+        initData();
+
     }
+
+    private void initData() {
+
+            RecycleViewBean bean=new RecycleViewBean();
+            bean.type=0;
+            list.add(bean);
+
+            RecycleViewBean bean2=new RecycleViewBean();
+            bean2.type=1;
+            list.add(bean2);
+
+            RecycleViewBean bean3=new RecycleViewBean();
+            bean3.type=1;
+            list.add(bean3);
+
+            RecycleViewBean bean4=new RecycleViewBean();
+            bean4.type=0;
+            list.add(bean4);
+            adapter.notifyDataSetChanged();
+    }
+
+    private void init() {
+        recyclerView= (RecyclerView) findViewById(R.id.recycle);
+      /*  LinearLayoutManager linear=new LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linear);*/
+        recyclerView.setLayoutManager(new GridLayoutManager(context,2));
+        adapter=new MyAdapter(context,list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(context,DividerItemDecoration.VERTICAL_LIST));
+    }
+
+
+
 }

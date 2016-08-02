166fcd34af212e3c44849861c778d4ad2cd66e8b
naruto2015 <995804286@qq.com>
2016-08-02 14:28:43 +0800

    gds

---
+++ app/src/main/java/com/myandroidview/view/canvas/PathViewOne.java
@@ -0,0 +1,24 @@
+package com.myandroidview.view.canvas;
+
+/**
+ * Created by naruto on 2016/8/2.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PathViewOne extends View{
+
+    public PathViewOne(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PathViewOne(Context context) {
+        super(context);
+    }
+
+
+
+
+}

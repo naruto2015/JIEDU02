ac477bc346975afaaf176c9f6b3a02b655ddab37
naruto2015 <995804286@qq.com>
2016-08-04 14:25:08 +0800

    gds

--- app/src/main/java/com/myandroidview/animation/ObjectAnimActivity.java
+++ app/src/main/java/com/myandroidview/animation/ObjectAnimActivity.java
@@ -1,9 +1,12 @@
 package com.myandroidview.animation;
 
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 
 import com.myandroidview.activity.R;
 
@@ -17,11 +20,39 @@ public class ObjectAnimActivity extends AppCompatActivity {
     }
 
 
-    public void rotateyAnimRun(View view){
-        ObjectAnimator//
-                .ofFloat(view, "rotationX", 0.0F, 360.0F)//
+    public void propertyvaluesholder(View view){
+
+        PropertyValuesHolder rotationHolder=PropertyValuesHolder.ofFloat("Rotation", 60f, -60f, 40f, -40f, -20f, 20f, 10f, -10f, 0f);
+        PropertyValuesHolder colorHolder = PropertyValuesHolder.ofInt("BackgroundColor", 0xffffffff, 0xffff00ff, 0xffffff00, 0xffffffff);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(view, rotationHolder, colorHolder);
+        animator.setDuration(3000);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.start();
+    }
+
+
+    public void rotateyAnimRun(final View view){
+       /* ObjectAnimator//
+                .ofFloat(view, "rotationY", 0.0F, 360.0F)//
                 .setDuration(500)//
-                .start();
+                .start();*/
+
+        ObjectAnimator anim = ObjectAnimator//
+                .ofFloat(view, "zhy", 1.0F,  0.0F)//
+                .setDuration(500);//
+        anim.start();
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+        {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation)
+            {
+                float cVal = (Float) animation.getAnimatedValue();
+                view.setAlpha(cVal);
+                view.setScaleX(cVal);
+                view.setScaleY(cVal);
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/com/myandroidview/animation/ValueAnimatorActivity.java
@@ -0,0 +1,17 @@
+package com.myandroidview.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myandroidview.activity.R;
+
+public class ValueAnimatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_value_animator);
+
+
+    }
+}
